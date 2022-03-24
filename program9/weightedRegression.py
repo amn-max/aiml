685af7e67a9315b28fa820c2b47c8d8a313308b5
amn-max <ayushmnaik@gmail.com>
2022-03-24 08:11:18 +0530

    waste

--- program9/weightedRegression.py
+++ program9/weightedRegression.py
@@ -1,21 +1,16 @@
 #you can use this program as well https://medium.com/100-days-of-algorithms/day-97-locally-weighted-regression-c9cfaff087fb
 #video https://www.youtube.com/watch?v=ZSWs9Kj_10I
 
-import operator
-from os import listdir
-import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np1
-import numpy.linalg as np
-from scipy.stats.stats import pearsonr
+import numpy as np
 import pdb
 def kernel(point, xmat, k):
-	m,n=np1.shape(xmat)  #size of matrix m
-	weights=np1.mat(np1.eye(m)) #np.eye returns mat with 1 in the diagonal 
+	m,n=np.shape(xmat)  #size of matrix m
+	weights=np.mat(np.eye(m)) #np.eye returns mat with 1 in the diagonal 
 	for j in range(m):
 		diff=point-xmat[j]
-		weights[j,j]=np1.exp(diff*diff.T/(-2.0*k**2))
+		weights[j,j]=np.exp(diff*diff.T/(-2.0*k**2))
 	return weights
 
 def localWeight(point,xmat,ymat,k):
@@ -24,22 +19,22 @@ def localWeight(point,xmat,ymat,k):
 	return W
 
 def localWeightRegression(xmat,ymat,k):
-	row,col=np1.shape(xmat) #return 244 rows and  2 columns
-	ypred=np1.zeros(row)
+	row,col=np.shape(xmat) #return 244 rows and  2 columns
+	ypred=np.zeros(row)
 	for i in range(row):
 		ypred[i]=xmat[i]*localWeight(xmat[i],xmat,ymat,k)
 	return ypred
 
 data=pd.read_csv('dataset.csv')
-bill=np1.array(data.total_bill)
-tip=np1.array(data.tip)
+bill=np.array(data.total_bill)
+tip=np.array(data.tip)
 
-mbill=np1.mat(bill)
-mtip=np1.mat(tip)
+mbill=np.mat(bill)
+mtip=np.mat(tip)
 
-mbillMatCol=np1.shape(mbill)[1] # 1 for vertical i.e columns
-onesArray=np1.mat(np1.ones(mbillMatCol))
-xmat=np1.hstack((onesArray.T,mbill.T)) #hstack concate horizontal lists it takes one value from the fist and one from the second  
+mbillMatCol=np.shape(mbill)[1] # 1 for vertical i.e columns
+onesArray=np.mat(np.ones(mbillMatCol))
+xmat=np.hstack((onesArray.T,mbill.T)) #hstack concate horizontal lists it takes one value from the fist and one from the second  
 print(xmat)
 
 ypred=localWeightRegression(xmat,mtip,2)
